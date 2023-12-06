7919f818c415cb6f88890fddcba1eafe1d80401b
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-12-06 17:38:08 +0100

    Creating kNN class

--- src/knn.py
+++ src/knn.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'Daniel Elisabethsønn Antonsen, UiT Arctic University of Norway'
+
+# Import libraries and modules
+import numpy as np
 
 
+class k_nearest:
 
-
+    def __init__(self, k):
+        self.k = k
 
 
 
---
+++ src/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+__author__ = 'Daniel Elisabethsønn Antonsen, UiT Arctic University of Norway'
+
