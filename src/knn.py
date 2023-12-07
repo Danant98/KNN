3b3d7d6d9571cfc17bce2301eac3806cf881461a
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-12-07 12:44:55 +0100

    update

--- src/knn.py
+++ src/knn.py
@@ -6,10 +6,27 @@ __author__ = 'Daniel Elisabethsønn Antonsen, UiT Arctic University of Norway'
 import numpy as np
 
 
-class k_nearest:
+class kNN:
 
-    def __init__(self, k):
+    def __init__(self, X:np.ndarray, k:int = 3):
+        self.X = self.normalize(X)
         self.k = k
+
+    
+    def normalize(self, X:np.ndarray):
+        """
+        Normalize output data
+        """
+        return (X - np.min(X, axis = 1)) / (np.max(X, axis = 1) - np.min(X, axis = 1))
+
+
+    def euclidean_dist(self, x1:np.ndarray, x2:np.ndarray):
+        """
+        Euclidean distance between two input points
+        """
+        return np.linalg.norm(x1 - x2)
+
+
 
 
 
