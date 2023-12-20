aab439a68adb4f036fa0449105ebee04f1a903e4
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-12-20 15:32:45 +0100

    Working k-nearest neighbour alg.thm

--- src/knn.py
+++ src/knn.py
@@ -8,25 +8,53 @@ import numpy as np
 
 class kNN:
 
-    def __init__(self, X:np.ndarray, k:int = 3):
-        self.X = self.normalize(X)
+    def __init__(self, k:int = 3):
         self.k = k
 
-    
     def normalize(self, X:np.ndarray):
         """
-        Normalize output data
+        Normalize output data, N(0, 1)
         """
-        return (X - np.min(X, axis = 1)) / (np.max(X, axis = 1) - np.min(X, axis = 1))
-
+        return (X - np.mean(X, axis = 0)) / np.std(X, axis = 0)
+    
+    def fit(self, X:np.ndarray, Y:np.ndarray):
+        """
+        Storing X and Y training data
+        """
+        self.X_train, self.Y_train = self.normalize(X), Y
 
     def euclidean_dist(self, x1:np.ndarray, x2:np.ndarray):
         """
-        Euclidean distance between two input points
+        Computing Euclidean distance between two points
         """
-        return np.linalg.norm(x1 - x2)
+        return np.sqrt(np.sum((x1 - x2)**2))
 
+    def closest_neighbors(self, X:np.ndarray):
+        """
+        Computing the closest neighbours and returns most occuring label
+        """
+        dist = np.zeros(self.X_train.shape[0])
+        for i in range(self.X_train.shape[0]):
+            dist[i] = self.euclidean_dist(X, self.X_train[i])
 
+        # Finding k closest neighbours
+        k_closest = np.argsort(dist)[:self.k]
 
-
-
+        # Counting the number of each label
+        label = np.bincount(self.Y_train[k_closest])
+        return np.argmax(label)
+    
+    def predict(self, X_test:np.ndarray):
+        """
+        Predicting test data
+        """
+        # Normalizing test data
+        X_norm = self.normalize(X_test)
+        
+        # Initializing labels array
+        Y_pred = np.zeros(X_test.shape[0])
+        
+        for i in range(X_norm.shape[0]):
+            Y_pred[i] = self.closest_neighbors(X_norm[i])
+        
+        return Y_pred
--- src/main.py
+++ src/main.py
@@ -2,3 +2,36 @@
 
 __author__ = 'Daniel Elisabethsønn Antonsen, UiT Arctic University of Norway'
 
+# Importing libraries and modules
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from knn import kNN as kn
+
+# Loading IRIS data set
+data = load_iris()
+X_train, X_test, Y_train, Y_test = train_test_split(
+    data['data'], data['target'], test_size = 0.3, random_state = 0)
+
+
+def accuracy(ypred:np.ndarray, y:np.ndarray, string:bool = False):
+    """
+    Computing the accuracy
+    """
+    assert len(ypred) == len(y), 'predicted labels and ground truth must be of equal length'
+
+    # Computing accuracy
+    acc = np.sum(ypred == y) / y.shape[0]
+    if string:
+        print(f'Accuracy = {100 * acc:.3f}%')
+        return
+    return acc
+
+
+if __name__ == '__main__':
+    pass
+    # Running k-nearest neighbors algorithm
+    model = kn(7)
+    model.fit(X_train, Y_train)
+    pred = model.predict(X_test)
+    accuracy(pred, Y_test, string = True)
